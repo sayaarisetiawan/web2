80d303fb1eb257ab7dc05a3d6836028fba276384
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-02-20 17:18:05 +0700

    belajar array

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+// Belajar Array
+$hari = array("Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu");
+$hari1= "Selasa";
+// var_dump($hari);
+echo "Hari ini hari " .$hari[5]. "<br>";
+echo "Hari ini hari " .$hari1. "<br>";
+
+?>
