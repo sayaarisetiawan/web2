46f2a4420898d037028c60f423da2ce986663fcc
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-02-21 22:05:21 +0700

    task 4

--- array2.php
+++ array2.php
@@ -12,12 +12,10 @@ $mahasiswa = ["Ari Setiawan", "22510010", "Sistem Informasi", "ari22510010@stima
 <body>
     <h1> Daftar Mahasiswa</h1>
     <ul>
-        <?php foreach($mahasiswa as $mhs): ?>
-            <li>
-                <?php echo $mhs;
-                ?>
-            </li>
-            <?php endforeach ?>
+      <li><?php echo $mahasiswa[0]; ?></li>
+      <li><?php echo $mahasiswa[1]; ?></li>
+      <li><?php echo $mahasiswa[2]; ?></li>
+      <li><?php echo $mahasiswa[3]; ?></li>
     </ul>
 </body>
 </html>
