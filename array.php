ca2d123c6d78508448cd236c4abc0c312e685fd5
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-02-21 21:20:21 +0700

    2

--- array.php
+++ array.php
@@ -5,5 +5,9 @@ $hari = array("Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu");
 echo $hari[0];
 echo "<br>";
 print_r($hari[2]);
+echo "<br>";
 
+// cara baru
+$bulan = ["januari", "februari", "maret"];
+print_r($bulan);
 ?>
